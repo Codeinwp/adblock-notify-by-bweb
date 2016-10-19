ad22f7ffb53d4b2d6f6c9fd0650083b06ae0514e
selu91 <cristea.marius.alexandru@gmail.com>
2016-10-20 00:54:18 +0300

    fixed travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,8 @@
 /* jshint node:true */
 /* global require */
-var timeGrunt = require('time-grunt');
-
 module.exports = function (grunt) {
     'use strict';
 
-    timeGrunt(grunt);
     var loader = require( 'load-project-config' ),
         config = require( 'grunt-plugin-fleet' );
     loader( grunt, config ).init();
